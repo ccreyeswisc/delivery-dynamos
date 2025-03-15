f6dee61ad2835dd27c872886acbcc12b0baa9fe0
cassette-axd <reyescaseym@gmail.com>
2025-03-15 17:32:41 -0500

    modified files

--- backend/database/connect_sqlite3.py
+++ backend/database/connect_sqlite3.py
@@ -19,13 +19,16 @@ if __name__ == "__main__":
 
     # Create the table from the xlsx file
     con, df = locations.create_table(local_xlsx_path, local_db_path)
+
+    for col in df:
+        print(col)
     
-    # Insert the data into the SQLite table
-    locations.insert_data(df, con)
+    # # Insert the data into the SQLite table
+    # locations.insert_data(df, con)
     
-    # Fetch and display locations data from SQLite
-    print("Locations data from the database:")
-    locations.fetch_data(con)
+    # # Fetch and display locations data from SQLite
+    # print("Locations data from the database:")
+    # locations.fetch_data(con)
     
     # Close the SQLite connection
     con.close()
