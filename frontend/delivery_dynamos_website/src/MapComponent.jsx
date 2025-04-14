7e994461d14fa62f269f7a02f8b0775b86c17f45
cassette-axd <reyescaseym@gmail.com>
2025-04-14 15:19:20 -0500

    resolved issues displaying user location on map

--- frontend/delivery_dynamos_website/src/MapComponent.jsx
+++ frontend/delivery_dynamos_website/src/MapComponent.jsx
@@ -15,13 +15,17 @@ const MapComponent = ({ routes }) => {
     };
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/receive-user-location', {
+      const response = await fetch('/receive-user-location', {
         method: 'POST',
+        // mode: 'cors',
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
       });
+      if (!response.ok) {
+        throw new Error(`Server error: ${response.status}`);
+      }
       const responseData = await response.json();
       console.log('Response from Python backend:', responseData);
     } catch (error) {
